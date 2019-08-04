b374c28e03d569e44a25cc801d7588b8c7b521a6
tdrobinson94 <tdrobinson94@gmail.com>
2019-08-04 15:10:47 -0400

    connected MySQL server

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
-
+const mysql = require('mysql');
 const app = express();
 
 app.use(express.json());
+
+const connection = mysql.createConnection({
+     //properties
+     host: 'pcadbinstance.cfnr5txp4upk.us-east-2.rds.amazonaws.com',
+     user: 'pcauser',
+     password: 'Jasmine12#',
+     database: 'Miranda Code'
+})
+
+connection.connect( (error) => {
+     if(!error) {
+          console.log('Error');
+     } else {
+          console.log('Connected');
+     }
+})
 
 const courses = [
      {id: 1, name: 'course1'},
